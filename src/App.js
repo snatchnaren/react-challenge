d84b3c52589694f2705e02f87e4482a16965540c
Naren <naren.sharmendran@gmail.com>
2020-01-21 17:11:41 +0530

    Initial ReactJS setup

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Haraeee RAMA!!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
